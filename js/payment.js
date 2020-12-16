436c87c08d42d8818889c0c3f30a86ba3e5fba4e
hithahareendran@gmail.com <hithahareendran@gmail.com>
2020-12-16 21:08:50 +0100

    tablet mode

--- js/payment.js
+++ js/payment.js
@@ -1,15 +1,17 @@
-const cartItems = [
-];
 
 //get items from storage, create cart table and add to cart and popup
 function initializeCart() {
   //get shows
-  let booking = JSON.parse(localStorage.getItem('order'));
+  let bookings = JSON.parse(localStorage.getItem('bookings'));
+  if (bookings === null) {
+    localStorage.setItem("bookings", JSON.stringify([]));
+    bookings = JSON.parse(localStorage.getItem('bookings'));
+  }
 
   //get or intialize cart from local storage
   var selected = JSON.parse(localStorage.getItem("menu-cart-items"));
   if (selected === null) {
-    localStorage.setItem("menu-cart-items", JSON.stringify(cartItems));
+    localStorage.setItem("menu-cart-items", JSON.stringify([]));
     selected = JSON.parse(localStorage.getItem("menu-cart-items"));
   }
 
@@ -24,21 +26,24 @@ function initializeCart() {
   document.getElementById('checkout-cart-popup').innerHTML = "";
 
   //if there is booking add it as the first item on the table
-  if (booking !== null) {
-    console.log(booking);
+  bookings.forEach(booking => {
     const template = document.getElementById('template-cart-item');
     const instance = document.importNode(template.content, true)
     instance.getElementById('itemImage').setAttribute('src', 'img/' + booking.image);
-    instance.getElementById('itemName').innerHTML = booking.name + '<br/>' + booking.date+"+"+booking.time;
-    instance.getElementById('itemCount').innerHTML = '('+booking.category +')' + booking.seatings;
+    instance.getElementById('itemName').innerHTML = booking.name + '<br/>' + booking.date + "+" + booking.time;
+    instance.getElementById('itemCount').innerHTML = '(' + booking.category + ')' + booking.seatings;
     instance.getElementById('itemPrice').innerHTML = booking.price + 'kr';
     let vat = (Number(booking.price) / (1 + (1 / 0.2))).toFixed(2);
     instance.getElementById('vat').innerHTML = vat + 'kr';
-    total += Number(booking.price);
-    instance.getElementById('total').innerHTML = Number(booking.price) + 'kr';
-    instance.getElementById('adjust').innerHTML = "<span class='badge bg-secondary rounded-pill' onclick='removeBooking()'><i class='fas fa-trash-alt'></i></span>";
+    let totalBookingPrice = (Number(booking.price) * Number(booking.seatings));
+    total += totalBookingPrice;
+    instance.getElementById('total').innerHTML = totalBookingPrice + 'kr';
+    instance.getElementById('itemRemove').style.display = 'none';
+    instance.getElementById('itemAdd').style.display = 'none';
+    instance.getElementById("performanceRemove").addEventListener('click', (event) => removeBooking(booking));
     cartTable.getElementById("cart-body").appendChild(instance);
-  }
+
+  });
 
   // add all menu item to the template
   selected.forEach(item => {
@@ -53,6 +58,7 @@ function initializeCart() {
     let totalPrice = Number(item.itemPrice) * Number(item.count)
     total += totalPrice;
     instance.getElementById('total').innerHTML = totalPrice + 'kr';
+    instance.getElementById('performanceRemove').style.display = 'none';
     instance.getElementById("itemAdd").addEventListener('click', (event) => addCount(item.itemId));
     instance.getElementById("itemRemove").addEventListener('click', (event) => removeCount(item.itemId));
     cartTable.getElementById("cart-body").appendChild(instance);
@@ -60,7 +66,7 @@ function initializeCart() {
   });
 
   //if the cart has any item add the table to the html cart/popup
-  if (selected.length > 0 || booking!==null) {
+  if (selected.length > 0 || bookings.length > 0) {
     document.getElementById('checkout-cart-popup').appendChild(cartTable.cloneNode(true));
     document.getElementById('checkout-cart').appendChild(cartTable);
     document.querySelectorAll('#grand-total').forEach(i => i.innerHTML = total + 'kr');
@@ -72,9 +78,10 @@ function initializeCart() {
 
 }
 
-function removeBooking()
-{
-  localStorage.removeItem('order');
+function removeBooking(existingBooking) {
+  let bookings = JSON.parse(localStorage.getItem('bookings'));
+  bookings = bookings.filter(booking => booking.orderId != existingBooking.orderId);
+  localStorage.setItem("bookings", JSON.stringify(bookings));
   initializeCart();
 }
 
@@ -132,6 +139,12 @@ function createAndShowModelPopup() {
   let email = document.getElementById('email').value;
   document.getElementById('email-comment').innerHTML = 'A reciept will be send to email ' + email +
     ' or an sms on number ' + document.getElementById('contact').value + " will be send to you.";
+  var list = document.getElementsByClassName("adjust");
+  for (let item of list) {
+    item.style.display = 'none';
+  }
+
+  //sendEmail(referenceNo, email);
 
 
   //show popup
@@ -145,11 +158,27 @@ function createAndShowModelPopup() {
 
 function checkoutComplete() {
   document.getElementById("payment-form").reset();
-  window.location.assign("/home.html");
+  window.location.assign("/index.html");
+}
+
+/*https://www.pepipost.com/tutorials/how-to-send-emails-with-javascript/ */
+function sendEmail(referenceNo, email) {
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: "circusfoodrestaurant@gmail.com",
+    Password: "circusfoodrestaurant123",
+    To: email,
+    From: "circusfoodrestaurant@gmail.com",
+    Subject: "Thank you for ordering food from circus food restaturant,  your order reference number #" + referenceNo,
+    Body: document.getElementById('email-content').innerHTML,
+  }).then(
+    message => console.log("mail sent successfully")
+  );
 }
 
 
 window.onload = () => {
+  createNav();
   initializeCart();
 }
 
@@ -171,10 +200,10 @@ window.onload = () => {
         }
         else {
           var selected = JSON.parse(localStorage.getItem("menu-cart-items"));
-          let booking = JSON.parse(localStorage.getItem('order'));
-          if (booking!=null || (selected != null && selected.length > 0)) {
+          let bookings = JSON.parse(localStorage.getItem('bookings'));
+          if ((bookings != null && bookings.length > 0) || (selected != null && selected.length > 0)) {
             console.log("valid");
-            localStorage.clear();
+            // localStorage.clear();
             //create popup and send email
             createAndShowModelPopup();
           }
--- js/tablet-menu.js
+++ js/tablet-menu.js
@@ -1,64 +1,170 @@
 function addItemsToMenu() {
-    items.forEach(item => {
-      let tab = document.getElementById(item.group);
-      const template = document.getElementById('template-menu-item');
-      const instance = document.importNode(template.content, true)
-      instance.getElementById('itemImage').setAttribute('src', 'img/' + item.image);
-      if(Number(item.rating) > 3)
-      {
-        instance.getElementById('discount').innerHTML = "";
-      }
-      for (let i = 0; i < 5; i++) {
-        if (i < item.rating)
-          instance.getElementById("stars").innerHTML += ' <a href="#"><i class="fas fa-star"></i></a>';
-        else
-          instance.getElementById("stars").innerHTML += ' <a href="#"><i class="far fa-star"></i></a>';
-      }
-      instance.getElementById('name').innerHTML = item.title;
-      if(Number(item.rating) > 3)
-      {
-        instance.getElementById('price').innerHTML = item.price + 'Kr';
-      }
-      else
-      {
-          let discountPrice = Math.round(0.67 * Number(item.price));
-          instance.getElementById('price').innerHTML = "<span>"+item.price + 'Kr'+"</span> "+discountPrice + 'Kr'+"";
-      }
-     instance.getElementById("add-to-cart").addEventListener('click', () => addItem(item.id, item.title, item.image, item.price));
-     instance.getElementById("remove-from-cart").addEventListener('click', () => removeItem(item.id, item.title, item.image, item.price));
-     tab.appendChild(instance);
-    })
+  //get or intialize cart from local storage
+  var selected = JSON.parse(localStorage.getItem("tab-menu-cart-items"));
+  if (selected === null) {
+    selected = [];
+    localStorage.setItem("tab-menu-cart-items", JSON.stringify(selected));
   }
+
+  items.forEach(item => {
+    let tab = document.getElementById(item.group);
+    tab.innerHTML="";
+  });
+
+  items.forEach(item => {
+    let tab = document.getElementById(item.group);
+    const template = document.getElementById('template-menu-item');
+    const instance = document.importNode(template.content, true)
+    instance.getElementById('itemImage').setAttribute('src', 'img/' + item.image);
+    if (Number(item.rating) > 3) {
+      instance.getElementById('discount').innerHTML = "";
+    }
+    for (let i = 0; i < 5; i++) {
+      if (i < item.rating)
+        instance.getElementById("stars").innerHTML += ' <a href="#"><i class="fas fa-star"></i></a>';
+      else
+        instance.getElementById("stars").innerHTML += ' <a href="#"><i class="far fa-star"></i></a>';
+    }
+    instance.getElementById('name').innerHTML = item.title;
+    let finalPrice = 0;
+    if (Number(item.rating) > 3) {
+      instance.getElementById('price').innerHTML = item.price + 'Kr';
+      finalPrice = item.price;
+    }
+    else {
+      let discountPrice = Math.round(0.67 * Number(item.price));
+      instance.getElementById('price').innerHTML = "<span>" + item.price + 'Kr' + "</span> " + discountPrice + 'Kr' + "";
+      finalPrice = discountPrice;
+    }
+    let existingItem = selected.find(element => element.itemId === item.id);
+    let count = existingItem ? existingItem.count : 0;
+    instance.getElementById("item-count-div").innerHTML = "<label id='" + item.id + "' > " + count + " </label>";
+    instance.getElementById("add-to-cart").addEventListener('click', () => addItem(item.id, item.title, item.image, finalPrice));
+    instance.getElementById("remove-from-cart").addEventListener('click', () => removeItem(item.id, item.title, item.image, finalPrice));
+    instance.getElementById("modal-link").addEventListener('click', () => openItemOnModal(item,finalPrice));
+    tab.appendChild(instance);
+  })
+}
+
+
+function openItemOnModal(item, price)
+{
+  var myModal = new bootstrap.Modal(document.getElementById('show-item'), {
+  })
+  myModal.show();
+
+  document.getElementById('modal-name').innerHTML = item.title;
+  document.getElementById('modal-price').innerHTML = price + 'Kr';
+  document.getElementById('modal-info').innerHTML = item.info;
+  document.getElementById('modal-content').innerHTML = item.content;
+  document.getElementById('modal-itemImage').setAttribute('src', 'img/' + item.image);
+  document.getElementById("modal-stars").innerHTML = "";
+     for (let i = 0; i < 5; i++) {
+      if (i < item.rating)
+      document.getElementById("modal-stars").innerHTML += ' <a href="#"><i class="fas fa-star"></i></a>';
+      else
+      document.getElementById("modal-stars").innerHTML += ' <a href="#"><i class="far fa-star"></i></a>';
+    }
+}
 
 function removeItem(itemId, itemName, itemImage, itemPrice) {
   var selected = JSON.parse(localStorage.getItem("tab-menu-cart-items"));
   let existingItem = selected.find(item => item.itemId === itemId);
   if (existingItem.count > 0) {
     existingItem.count--;
+    document.getElementById(itemId).innerHTML = existingItem.count;
   }
   localStorage.setItem("tab-menu-cart-items", JSON.stringify(selected));
-  refreshItems();
+  calculateTotalPrice();
+  fillCart();
+}
+
+function addItem(itemId, itemName, itemImage, itemPrice) {
+  var selected = JSON.parse(localStorage.getItem("tab-menu-cart-items"));
+  let existingItem = selected.find(item => item.itemId === itemId);
+  if (existingItem) {
+    existingItem.count++;
+    document.getElementById(itemId).innerHTML = existingItem.count;
+  }
+  else {
+    selected.push({ itemId: itemId, itemName: itemName, itemImage: itemImage, itemPrice: itemPrice, count: 1 });
+    document.getElementById(itemId).innerHTML = 1;
+  }
+  localStorage.setItem("tab-menu-cart-items", JSON.stringify(selected));
+  calculateTotalPrice();
+  fillCart();
+}
+
+function calculateTotalPrice() {
+  let total = 0, totalCount = 0;
+  var selected = JSON.parse(localStorage.getItem("tab-menu-cart-items"));
+  selected.forEach(item => {
+      total += (Number(item.itemPrice) * Number(item.count));
+      totalCount += item.count;
+    });
+    document.getElementById("item-total").innerHTML="items("+totalCount+")";
+    document.getElementById("item-price").innerHTML="price "+total+" kr";
+}
+
+// get the items from local storage and add to cart
+function fillCart() {
+  //get or intialize cart from local storage
+  var selected = JSON.parse(localStorage.getItem("tab-menu-cart-items"));
+  if (selected === null) {
+    selected = [];
+    localStorage.setItem("tab-menu-cart-items", JSON.stringify(selected));
   }
 
-  function addItem(itemId, itemName, itemImage, itemPrice) {
-    var selected = JSON.parse(localStorage.getItem("tab-menu-cart-items"));
-    let existingItem = selected.find(item => item.itemId === itemId);
-    if (existingItem) {
-       existingItem.count++;
-     }
-    else {
-      selected.push({ itemId: itemId, itemName: itemName, itemImage: itemImage, itemPrice: itemPrice, count: 1 });
-     }
-     localStorage.setItem("tab-menu-cart-items", JSON.stringify(selected));
-     refreshItems();
-  }
-
-  function refreshItems()
-  {
+  //clean the cart
+  document.getElementById('itemList').innerHTML = "";
+  document.getElementById("checkout-button").className = 'btn btn-primary invisible button';
     
+  let total=0, totalcount=0;
+  //add each menu item to cart
+  selected.forEach(item => {
+    const template = document.getElementById('template-cart-item');
+    const instance = document.importNode(template.content, true)
+    instance.getElementById('itemName').innerHTML = item.itemName;
+    instance.getElementById('itemCount').innerHTML = item.count;
+    instance.getElementById('itemPrice').innerHTML = item.itemPrice + 'kr';
+    instance.getElementById('itemImage').setAttribute('src', 'img/' + item.itemImage);
+    document.getElementById('itemList').appendChild(instance);
+    document.getElementById("checkout-button").className = 'btn btn-primary visible button';
+    total += (Number(item.itemPrice) * item.count);
+    totalcount += item.count;
+  })
+
+  if (!selected || !selected.length)
+  {
+    document.getElementById('itemList').innerHTML = '<li class="list-group-item"><div>Empty</div></li>';
+  }
+  else{
+    const template = document.getElementById('template-cart-item');
+    const instance = document.importNode(template.content, true)
+    instance.getElementById('itemName').innerHTML = "<b>Total</b>";
+    instance.getElementById('itemCount').innerHTML = totalcount;
+    instance.getElementById('itemPrice').innerHTML = total + 'kr';
+    instance.getElementById('itemImage').style.display = "none";
+    document.getElementById('itemList').appendChild(instance);
   }
 
 
-  window.onload = () => {
-    addItemsToMenu();
-  }
+}
+
+function doCheckout()
+{
+  alert('Thank you for ordering, your order successfullty placed');
+  var selected = JSON.parse(localStorage.getItem("tab-menu-cart-items"));
+  selected.forEach(item => document.getElementById(item.itemId).innerHTML = 0);
+  localStorage.clear();
+  addItemsToMenu();
+  calculateTotalPrice();
+  fillCart();
+}
+
+
+window.onload = () => {
+  addItemsToMenu();
+  calculateTotalPrice();
+  fillCart();
+}
